9518be7c3e0561856ee092e11acc2da46a6dfece
James <james@lois.fritz.box>
2024-10-01 22:19:09 +0100

    Remove old test code from Wendy

--- opt_bob/wendy/wendy.py
+++ opt_bob/wendy/wendy.py
@@ -2,28 +2,8 @@
 
 from flask import Flask, redirect
 import os
-import threading
-import time
 
 app = Flask('wendy')
-
-
-def follow_log(filename):
-    print(f"Following {filename}")
-    thefile = open(filename, 'rt')
-    thefile.seek(0, os.SEEK_END)
-    watermark = thefile.tell()
-    while True:
-        time.sleep(0.5)
-        # try and seek
-        thefile.seek(0, os.SEEK_END)
-        new_end = thefile.tell()
-        if watermark != new_end:
-            thefile.seek(watermark)
-            while watermark < new_end:
-                line = thefile.readline()
-                print(line, end='')
-                watermark = thefile.tell()
 
 
 @app.route("/")
@@ -36,11 +16,6 @@ def ping_pong():
     return "pong\n"
 
 
-@app.route("/file/<path:file_path>")
-def get_file(file_path):
-    return redirect(f"http://localhost/{file_path}", code=308)
-
-
 @app.route("/api/complete/<mac>")
 def complete_host(mac):
     os.system(f'/usr/local/sbin/bob complete {mac}')
@@ -48,8 +23,6 @@ def complete_host(mac):
 
 
 if __name__ == '__main__':
-    x = threading.Thread(target=follow_log, args=('/var/log/messages',))
-    x.start()
     # export FLASK_DEBUG=1
     app.run(port='5000', debug=True )
     # app.run(host='0.0.0.0')
